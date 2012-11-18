948dbd7f70b7886e2d3be885c5f425e8313afe3d
Neavo <neavo7@Gmail.com>
2012-11-18 14:11:23 +0800

    fix

--- assets/www/app/view/container/newsDetailMain.js
+++ assets/www/app/view/container/newsDetailMain.js
@@ -2,8 +2,6 @@ Ext.define("Project.view.container.newsDetailMain", {
 	extend : "Ext.Container",
 	xtype : "newsDetailMain",
 	config : {
-		width : Ext.Viewport.getWindowWidth(),
-		height : Ext.Viewport.getWindowHeight(),
 		scrollable : "vertical",
 	},
 });
