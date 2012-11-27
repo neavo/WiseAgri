1170d30297700a90fc5e2a77378d60baadd2615c
Neavo <neavo7@Gmail.com>
2012-11-27 13:54:56 +0800

    1. 添加List属性 "emptyText" 与 "loadingText" 2. 调整 信息列表 样式

--- assets/www/app/view/container/categoryListMain.js
+++ assets/www/app/view/container/categoryListMain.js
@@ -3,8 +3,10 @@ Ext.define("Project.view.container.categoryListMain", {
 	xtype : "categoryListMain",
 	config : {
 		store : "categoryListStore",
+		emptyText : "没有更多信息 ...",
+		loadingText : "正在获取信息...",
 		itemTpl : "<img class = categoryListIcon src = {categoryIconUrl} />"
 		 + "<img class = categoryListDisclosure src = resources/icons/disclosureIcon.png />"
-		 + "<div class = categoryListName><b>{categoryName} </b></div>",
+		 + "<div class = categoryListName><b>{categoryName}</b></div>",
 	},
 });
--- assets/www/app/view/container/newsListMain.js
+++ assets/www/app/view/container/newsListMain.js
@@ -3,8 +3,11 @@ Ext.define("Project.view.container.newsListMain", {
 	xtype : "newsListMain",
 	config : {
 		store : "newsStore",
+		emptyText : "没有更多信息 ...",
+		loadingText : "正在获取信息...",
 		itemTpl : "<img class = newsListIcon onerror = \" this.src = 'resources/icons/defaultIcon.png' \" src = {iconUrl} />"
 		 + "<div class = newsListTitle><b>{newsTitle}</b></div>"
-		 + "<div class = newsListDatePubliser>{dateTime}　来源：{newsPublisher}</div>",
+		 + "<div class = newsListDatePubliser>时间：{dateTime}</div>"
+		 + "<div class = newsListDatePubliser>来源：{newsPublisher}</div>",
 	},
 });
